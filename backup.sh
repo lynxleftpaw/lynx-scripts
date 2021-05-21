63922da176c3a459b160c8f98ee5e5828b65199f
Lynx Leftpaw <lynx@k10.co>
2021-05-20 19:28:17 -0500

    rsync backup script

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#rsync -av --delete /home /media/lynx/soggy/_backup/lgbrr/home
+BKUP_DEST="/your/target/location"
+
+# full system archive, ignore extra partitions, include sym/hard links, exclude various caches, show progress / transfer speed
+rsync -aAXHSx --info=progress2 --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*/.gvfs","/home/*/.cache/mozilla/*","/home/*/.cache/BraveSoftware"} / $BKUP_DEST
